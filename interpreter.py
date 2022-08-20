62d1b22ab9569b7c32b025eac650cc6a1c012d51
jonasbrocas <55618670+jonasbrocas007@users.noreply.github.com>
2022-08-20 14:22:07 +0100

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -28,7 +28,6 @@ goto_checker = '&'
 allvarlist = []
 rows = []
 read = f.readlines()
-
 for checker in read:
     read_header += 1
     readline = read[read_header]
@@ -38,7 +37,6 @@ for checker in read:
         print(readpoint)
         if '>' in readline:
             print('debug,yes')
-
 for op_checker in read:
     read_op += 1
     readline = read[read_op]
@@ -80,7 +78,6 @@ for op_checker in read:
             x = div[0]
             y = div[1]
             print(eval('x / y'))
-
 for var_checker in read:
     read_var += 1
     readline = read[read_var]
@@ -101,13 +98,11 @@ for var_checker in read:
             allvarlist = allvarlist.replace("'",'')
             allvarlist = allvarlist.replace(' ','')
             allvarlistf = allvarlist.split(',')
-        
+
 for cout_checker in read:
     read_cout += 1
     readline = read[read_cout]
     if readline[0] == '>':
-        findvar += 1
-        findlist += 1
         readline = readline.replace('\n','')
         readline = readline.split('>')
         varprint = allvarlist.index(readline[1])
@@ -128,48 +123,47 @@ for if_checker in read:
         readvarcheck = allvarlist.index(readvar[0])
         if readvar[readvarcheck] == allvarlistf[readvarcheck]:
             if readvar[readvarcheck + 1] == allvarlistf[readvarcheck + 1]:
-                    fread_op += 1
-                    readline = read[fread_op]
-                    if 'op' in readif[1]:
-                        countprep = str(readif[1])
-                        countprep = countprep.replace('op', '')
-                        countprep = countprep.replace(' ', '')
-                        countprep = countprep.replace('\n', '')
-                        if '+' in countprep:
-                            add = countprep.replace(' ', '')
-                            add = add.split('+')
-                            add = map(int,add)
-                            add = list(add)
-                            x = add[0]
-                            y = add[1]
-                            print(eval('x + y'))
-                        if '-' in countprep:
-                            sub = countprep.replace(' ', '')
-                            sub = sub.split('-')
-                            sub = map(int,sub)
-                            sub = list(sub)
-                            x = sub[0]
-                            y = sub[1]
-                            print(eval('x - y'))
-                        if 'x' in countprep:
-                            mul = countprep.replace(' ', '')
-                            mul = mul.split('x')
-                            mul = map(int,mul)
-                            mul = list(mul)
-                            x = mul[0]
-                            y = mul[1]
-                            print(eval('x * y'))
-                        if '/' in countprep:
-                            div = countprep.replace(' ', '')
-                            div = div.split('/')
-                            div = map(int,div)
-                            div = list(div)
-                            x = div[0]
-                            y = div[1]
-                            print(eval('x / y'))
+                fread_op += 1
+                readline = read[fread_op]
+                if 'op' in readif[1]:
+                    countprep = str(readif[1])
+                    countprep = countprep.replace('op', '')
+                    countprep = countprep.replace(' ', '')
+                    countprep = countprep.replace('\n', '')
+                    if '+' in countprep:
+                        add = countprep.replace(' ', '')
+                        add = add.split('+')
+                        add = map(int,add)
+                        add = list(add)
+                        x = add[0]
+                        y = add[1]
+                        print(eval('x + y'))
+                    if '-' in countprep:
+                        sub = countprep.replace(' ', '')
+                        sub = sub.split('-')
+                        sub = map(int,sub)
+                        sub = list(sub)
+                        x = sub[0]
+                        y = sub[1]
+                        print(eval('x - y'))
+                    if 'x' in countprep:
+                        mul = countprep.replace(' ', '')
+                        mul = mul.split('x')
+                        mul = map(int,mul)
+                        mul = list(mul)
+                        x = mul[0]
+                        y = mul[1]
+                        print(eval('x * y'))
+                    if '/' in countprep:
+                        div = countprep.replace(' ', '')
+                        div = div.split('/')
+                        div = map(int,div)
+                        div = list(div)
+                        x = div[0]
+                        y = div[1]
+                        print(eval('x / y'))
                     if '.' in readif[1]:
                         print(readif[1])
-
 for end_checker in read:
     read_end += 1
     readline = read[read_end]
@@ -182,13 +176,13 @@ for goto_checker in read:
     if readline[0] == '&':
         goto = readline.replace('\n','')
         goto = goto.split('&')
-        goto_var = goto[1] 
-        read_header = goto_var 
-        read_op = goto_var 
-        read_end = goto_var 
-        read_var = goto_var 
-        read_cout = goto_var 
-        var_count = goto_var 
+        goto_var = int(goto[1])
+        read_header = goto_var
+        read_op = goto_var
+        read_end = goto_var
+        read_var = goto_var
+        read_cout = goto_var
+        var_count = goto_var
         if_count = goto_var
-        
+        read_goto = goto_var
     
